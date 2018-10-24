49ddd75345937bc9db663ddb6a3ffb972af554af
leonardo.catanante <leonardo.catanante@AN085283.anvisa.gov.br>
2018-10-24 12:59:05 -0200

    a

--- EstruturaDeDados/src/com/vunter/EstruturaDeDados/Vetor.java
+++ EstruturaDeDados/src/com/vunter/EstruturaDeDados/Vetor.java
@@ -10,17 +10,9 @@ public class Vetor {
 		this.elementos = new Object[capacidade];
 		this.tamanho = 0;
 	}
-	/*
-	 * public void adiciona(String elemento) { for (int i = 0; i <
-	 * this.elementos.length; i++) { if (this.elementos[i] == null) {
-	 * this.elementos[i] = elemento; break; }
-	 * 
-	 * }
-	 * 
-	 * Método acima funciona, mas não é tão eficiente.
-	 */
 
 	public boolean adiciona(Object elemento) {
+		this.aumentaCapacidade();
 		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
 			this.tamanho++;
@@ -30,6 +22,7 @@ public class Vetor {
 	}
 
 	public boolean adiciona(Object elemento, int posicao) {
+		this.aumentaCapacidade();
 		if (posicao == tamanho) { // Verifica se a posição é pra inserir no ultimo do vetor
 			System.out.println("Ultimo item!");
 			this.adiciona(elemento);
@@ -50,8 +43,42 @@ public class Vetor {
 		return true;
 	}
 
+	public void remove(int posicao) {
+		if (!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posição Inválida!");
+		}
+		for (int i = posicao; i < tamanho - 1; i++) {
+			this.elementos[i] = this.elementos[i + 1];
+		}
+		this.tamanho--;
+	}
+
+	public void removePorNome(Object elemento) {
+		int posicao = -1;
+		for (int i = 0; i < this.tamanho; i++) {
+			if (this.elementos[i].equals(elemento)) {
+				posicao = i;
+			}
+		}
+		if (posicao >= 0) {
+			this.remove(posicao);
+		} else {
+			throw new IllegalArgumentException("Elemento não existe!");
+		}
+	}
+
 	public int tamanho() {
 		return this.tamanho;
+	}
+
+	private void aumentaCapacidade() {
+		if (this.tamanho == this.elementos.length) {
+			Object[] elementosNovos = new Object[this.elementos.length * 2];
+			for (int i = 0; i < elementos.length; i++) {
+				elementosNovos[i] = this.elementos[i];
+			}
+			this.elementos = elementosNovos;
+		}
 	}
 
 	public Object busca(int posicao) { // Busca por posição
--- EstruturaDeDados/src/com/vunter/EstruturaDeDados/main/Aula03.java
+++ EstruturaDeDados/src/com/vunter/EstruturaDeDados/main/Aula03.java
@@ -5,7 +5,7 @@ import com.vunter.EstruturaDeDados.Vetor;
 public class Aula03 {
 
 	public static void main(String[] args) {
-		Vetor vetor = new Vetor(10);
+		Vetor vetor = new Vetor(2);
 
 		vetor.adiciona("Elemento 1");
 		vetor.adiciona("Elemento 2");
@@ -14,11 +14,18 @@ public class Aula03 {
 		System.out.println(vetor);
 		System.out.println(vetor.busca(2));
 		System.out.println("Elemento na Posição: " + vetor.busca("Elemento 2"));
-		
+
 		vetor.adiciona("A", 0);
 		vetor.adiciona("B", 4);
-		
+
 		System.out.println(vetor);
+
+		vetor.remove(1);
+		System.out.println(vetor);
+
+		vetor.removePorNome("Elemento 3");
+		System.out.println(vetor);
+
 	}
 
 }
